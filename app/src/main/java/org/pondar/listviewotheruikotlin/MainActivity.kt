b165475d006eed294bbf79227ddea23271106ff8
makn <makn@eaaa.dk>
2018-08-28 13:23:05 +0200

    Initial commit

---
+++ app/src/main/java/org/pondar/listviewotheruikotlin/MainActivity.kt
@@ -0,0 +1,44 @@
+package org.pondar.listviewotheruikotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val countries = arrayOf("Albania", "Algeria", "Armenia", "Andora", "Angola", "Argentina", "Australia", "Bahrain",
+            "Bangladesh", "Barbados", "Brazil", "China", "Denmark", "Egypt", "France", "Ghana", "Hong Kong", "India", "Italy",
+            "United Kingdom", "United States", "United Arab Emirates")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_checked, countries)
+
+        //val listView = mylist
+        mylist.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+        //Button clicked
+        button.setOnClickListener{
+            val selected = mylist.checkedItemPosition
+            Log.d("selection", "selected: $selected")
+            if (selected != ListView.INVALID_POSITION)
+                Toast.makeText(applicationContext,
+                        "My selection: " + countries[selected], Toast.LENGTH_SHORT).show()
+        }
+
+        mylist.adapter = adapter
+        mylist.setOnItemClickListener { parent, view, position, id ->
+            Log.d("listview", "itemclicked")
+            val country = countries[position]
+            Toast.makeText(applicationContext, "you clicked : $country", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
