13430623556d561c01c2cd5e1a74379adc455452
zaifrun <martinknud@gmail.com>
2020-09-09 08:16:14 +0200

    added array in strings.xml

--- app/src/main/java/org/pondar/listviewotheruikotlin/MainActivity.kt
+++ app/src/main/java/org/pondar/listviewotheruikotlin/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     //normally you should not hardcode this, but keep in the strings.xml as a string array.
-    private val countries = arrayOf("Albania", "Algeria", "Armenia", "Andora", "Angola", "Argentina", "Australia", "Bahrain",
+    private val countries = mutableListOf("Albania", "Algeria", "Armenia", "Andora", "Angola", "Argentina", "Australia", "Bahrain",
             "Bangladesh", "Barbados", "Brazil", "China", "Denmark", "Egypt", "France", "Ghana", "Hong Kong", "India", "Italy",
             "United Kingdom", "United States", "United Arab Emirates")
 
@@ -20,8 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = ArrayAdapter<String>(this,
+        var adapter = ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_checked, countries)
+
+        //below is a way of initializing the adapter with something defined in the strings.xml
+       /* adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_checked,
+                    resources.getStringArray(R.array.countries2)
+                )*/
 
         mylist.choiceMode = ListView.CHOICE_MODE_SINGLE
 
@@ -40,5 +45,10 @@ class MainActivity : AppCompatActivity() {
             val country = countries[position]
             Toast.makeText(applicationContext, "you clicked : $country", Toast.LENGTH_SHORT).show()
         }
+
+        countries.add("United Nations")
+
+
+
     }
 }
